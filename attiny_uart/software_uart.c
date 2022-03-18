42d9be0b8c07648f276b36169f16bc4171057bd2
Haluk Simsek <37951127+haluks@users.noreply.github.com>
2022-03-18 11:33:22 +0300

    Add files via upload

---
+++ attiny_uart/software_uart.c
@@ -0,0 +1,101 @@
+/*
+ * software_uart.c
+ *
+ * Created: 16.03.2022 15:10:12
+ *  Author: haluk
+ */ 
+#include "software_uart.h"
+#include <util/delay_basic.h>
+volatile uint8_t rx_bas=0,rx_son=0;
+volatile uint8_t rx_ring[UART_Rx_Boyut];
+int16_t tx_delay=0;
+int16_t rx_delay=0;
+int16_t yarim_rx_delay=0;
+int16_t stop_rx_delay=0;
+volatile uint8_t veri=0;
+ISR(PCINT0_vect){
+	if (RX_PIN_LOW){
+		_delay_loop_2(yarim_rx_delay);
+		for (uint8_t i=0;i<8;i++){			
+			_delay_loop_2(rx_delay);
+			veri>>=1;
+			//PORTB^=(1<<4);
+			if (RX_PIN_HIGH){
+				veri|=0x80 ;
+				
+				}else{
+				veri&=~0x80 ;				
+			}			
+		}
+		_delay_loop_2(stop_rx_delay);		
+		rx_bas=(rx_bas+1) & UART_Rx_Mask;
+		rx_ring[rx_bas]=veri;
+	}	
+}
+
+void uart_basla(Bd_rate_t _baud){
+	cli();
+	RX_HIGH;
+	RX_IN;
+	TX_HIGH;
+	TX_OUT;
+	PCMSK|=(1<<PCINT3);//pb3
+	GIMSK|=(1<<PCIE);
+	tx_delay = ((F_CPU / _baud) / 4)-3;
+	yarim_rx_delay=(F_CPU/_baud/8)-10;
+		if (yarim_rx_delay<=1){
+			yarim_rx_delay=1;
+		}
+	rx_delay=(F_CPU/_baud/4)-5;
+		if (rx_delay<=1){
+			rx_delay=1;
+		}
+		stop_rx_delay=(F_CPU/_baud*3/16)-4;
+		if (stop_rx_delay<=1){
+			stop_rx_delay=1;
+		}
+	
+	sei();
+}
+
+uint8_t uart_oku(){
+	
+	rx_son=(rx_son+1) & UART_Rx_Mask;
+	return rx_ring[rx_son];
+}
+void uart_gonder(uint8_t uData){	
+	cli();
+	TX_LOW;
+	_delay_loop_2(tx_delay);
+	for (uint8_t i=0;i<8;i++){
+		if (uData&0x01){
+			TX_HIGH;
+			}else{
+			TX_LOW;
+		}
+		_delay_loop_2(tx_delay);
+		uData>>=1;
+	}
+	TX_HIGH;
+	_delay_loop_2(tx_delay);
+	sei();
+}
+void uart_dizi(const char *str){
+	while(*str){
+		uart_gonder(*str++);
+	}
+	
+}
+uint8_t uart_gelen(){
+	
+	if (rx_son==rx_bas)	return 0;
+	return 1;
+}
+void uart_dizi_al(char *stri){
+	uint8_t poz=0;
+	do{
+		stri[poz]=uart_oku();
+		poz++;
+	} while (!(rx_bas==rx_son));
+	stri[poz]='\0';
+}
---
+++ attiny_uart/software_uart.h
@@ -0,0 +1,56 @@
+/*
+ * software_uart.h
+ *
+ * Created: 16.03.2022 15:09:54
+ *  Author: haluk
+ */ 
+
+
+#ifndef SOFTWARE_UART_H_
+#define SOFTWARE_UART_H_
+
+#define F_CPU 8000000ul
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define TX_DDR			DDRB
+#define TX_PORT			PORTB
+#define TX_PIN			PINB
+
+#define RX_DDR			DDRB
+#define RX_PORT			PORTB
+#define RX_PIN			PINB
+
+#define TX_				PB4
+#define RX_				PB3
+#define TX_OUT			TX_DDR|=(1<<TX_)
+#define TX_HIGH			TX_PORT|=(1<<TX_)
+#define TX_LOW			TX_PORT&=~(1<<TX_)
+#define RX_IN			RX_DDR&=~(1<<RX_)
+#define RX_HIGH			RX_PORT|=(1<<RX_)
+#define RX_LOW			RX_PORT&=~(1<<RX_)
+#define RX_PIN_HIGH		RX_PIN&(1<<RX_)
+#define RX_PIN_LOW		!(RX_PIN&(1<<RX_))
+
+/////////////////////////////////
+#define UART_Rx_Boyut 64
+#define UART_Rx_Mask (UART_Rx_Boyut-1)
+typedef enum{
+	Bd4800			=4800,
+	Bd9600			=9600,
+	Bd19200			=19200,
+	Bd38400			=38400,
+	Bd57600			=57600,
+	Bd115200		=115200,//RX hata yüksek
+	Bd230400		=230400,//ilk biti sinirda yakaliyor RX çok kötü	
+}Bd_rate_t;
+
+////////////////////////////////////////////////////
+void uart_basla(Bd_rate_t baud);
+uint8_t uart_oku();
+void uart_gonder(uint8_t uData);
+void uart_dizi(const char *str);
+uint8_t uart_gelen();
+void uart_dizi_al( char *stri);
+
+#endif 
